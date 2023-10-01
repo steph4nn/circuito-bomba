d102e4a06e553e6fc3ad1139267cf6b2b6860b85
steph4nn <110504296+steph4nn@users.noreply.github.com>
2023-10-01 17:03:44 -0300

    Criando algoritmo base do jogo

---
+++ lista.py
@@ -0,0 +1,144 @@
+class Node:
+    '''
+    Classe de objetos para um nó dinâmico na memória
+    '''
+    def __init__(self,data):
+        self.__data = data
+        self.__next = None
+    
+    @property
+    def data(self):
+        return self.__data
+    
+    @data.setter
+    def data(self, newData):
+        self.__data = newData
+
+    @property
+    def next(self):
+        return self.__next
+    
+    @next.setter
+    def next(self, newNext):
+        self.__next = newNext
+
+    def hasNext(self):
+        return self.__next != None
+    
+    def __str__(self):
+        return str(self.__data)
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.__head = None
+        self.__tail = None
+        self.__size = 0
+    
+    def isEmpty(self):
+        return self.__size == 0
+    
+    def __len__(self):
+        return self.__size
+    
+    def insert(self, value, position):
+        if self.isEmpty():
+            new = Node(value)
+            self.__head = new
+            self.__tail = new
+            self.__tail.next = self.__head
+            self.__size+=1
+        elif position == 1:
+            new = Node(value)
+            new.next = self.__head
+            self.__head = new
+            self.__tail.next = self.__head
+            self.__size +=1
+        elif position == len(self)+1:
+            new = Node(value)
+            self.__tail.next = new
+            self.__tail = new
+            self.__tail.next = self.__head
+            self.__size+=1
+        else:
+            new = Node(value)
+            pointer = self.__head
+            count = 1
+            while (count < (position-1)):
+                pointer = pointer.next
+                count+=1
+            new.next = pointer.next
+            pointer.next = new
+            self.__size+=1
+
+    def advance(self,start ,quantity):
+        pointer = self.__head
+        count = 1
+        while (count<start):
+            pointer = pointer.next
+            count+=1
+        for i in range(quantity):
+            pointer = pointer.next
+        return pointer.data
+    
+    def remove(self,position):
+        if self.isEmpty():
+            return 'lista vazia'
+        elif position == 1:
+            data = self.__head.data
+            pointer = self.__head
+            self.__tail.next = pointer.next
+            self.__head = pointer.next
+            self.__size-=1
+            return data
+        elif position == len(self):
+            data = self.__tail.data
+            count = 1
+            pointer = self.__head
+            while count <position-1:
+                pointer = pointer.next
+                count+=1
+            pointer.next = self.__head
+            self.__tail = pointer
+            self.__size-=1
+            return data
+        else:
+            pointer = self.__head
+            ant =  self.__head
+            count = 1
+            while (count < position):
+                ant = pointer
+                pointer = pointer.next
+                count+=1
+            ant.next = pointer.next
+            data = pointer.data
+            self.__size-=1
+            return data
+
+    def index(self, elem):
+        assert not self.isEmpty(), 'Lista vazia'
+        pointer = self.__head
+        for i in range(len(self)):
+            if pointer.data == elem:
+                return i
+            else:
+                pointer =pointer.next
+
+    def element(self, index):
+        pointer = self.__head
+        count = 1
+        while(count <= index):
+            pointer = pointer.next 
+            count+=1
+        return pointer.data
+    
+    def __str__(self) -> str:
+        str = '[ '
+        if self.isEmpty():
+            str+= ']'
+            return str
+        pointer = self.__head
+        for i in range(1,len(self)+1):
+            str += f'{pointer.data}, '
+            pointer = pointer.next
+        str = str[:-2] + " ]"
+        return str
--- main.py
+++ main.py
@@ -1,17 +1,36 @@
-from listaEncadeadaC import *
+from lista import *
+from random import randint
 
-lista = Lista()
-lista.inserir(1,10)
-lista.inserir(1,20)
-lista.inserir(1,50)
-lista.inserir(4,30)
-lista.inserir(4,70)
+num_participantes = int(input())
+num_vencedores = int(input())
 
-
-print(lista)
-print(lista.index(30))
+assert num_participantes > 0
+assert num_vencedores < num_participantes and num_vencedores>0
 
 
+participantes = LinkedList()
+for i in range(num_participantes):
+    participante = input('Nome do participante: ')
+    posicao = int(input('Posição a ser inserido: '))
+    participantes.insert(participante,posicao)
 
-
+print(f'Particpantes Iniciais: {participantes}')
+start = randint(1,num_participantes)
+rodada = 1
+while len(participantes) != num_vencedores:   
+        music = randint(4,15)
+        print(f"Participantes: {participantes}")
+        print(f"Rodada: {rodada}")
+        print(f"Start: {participantes.element(start-1)} K={music}")
+        eliminado = participantes.advance(start,music)
+        posicaoEliminado = participantes.index(eliminado)+1
+        prox= participantes.advance(posicaoEliminado,1)
+        removido =participantes.remove(posicaoEliminado)
+        print(f"Removido: {removido}")
+        
+        start = participantes.index(prox)+1
+        
+        print('---------------FIM DO ROUND---------------------')
+        rodada+=1
+print(f"O(s) participante(s) vencedor(es): {participantes}")
 
